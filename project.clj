4f4ea9a7bf0061cfec41b10104fffc69a10c9969
Matthew Boston <matthew.boston@gmail.com>
2011-05-24 17:43:33 -0400

    0.2.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject bostonaholic/utils "0.1.0-SNAPSHOT"
+(defproject bostonaholic/utils "0.2.0-SNAPSHOT"
   :description "A collection of common tools."
   :dependencies [[org.clojure/clojure "1.2.1"]]
   :dev-dependencies [[speclj "1.4.0"]]
